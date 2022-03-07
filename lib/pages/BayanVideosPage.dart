10edde169b211dbd85d6020a583ca46c41ba7aaa
saadmansur <saad.mansur@sky.co.nz>
2022-03-07 23:15:32 +1300

    created video lectures page and vidoe playback logic.

--- lib/pages/BayanVideosPage.dart
+++ lib/pages/BayanVideosPage.dart
@@ -92,7 +92,7 @@ class _MyHomePageState extends State<BayanVideosPage> {
                         ),
                       )
                   ),
-                )
+                );
               },
             );
           }
